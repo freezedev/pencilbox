4b8a1519cf914805d5821c8aba34b4d0951ffeee
Johannes Stein <johannesstein@freeze-dev.com>
2013-09-07 15:54:54 +0200

    Added dist JS

---
+++ dist/pencilbox.js
@@ -0,0 +1,32 @@
+(function() {
+  (function() {})();
+
+}).call(this);
+
+(function() {
+
+
+}).call(this);
+
+(function() {
+  udefine('pencilbox/interface/methods', function() {
+    var methods;
+    return methods = ['drawTexture', 'drawRect', 'drawCircle', 'drawLine', 'drawPoint'];
+  });
+
+}).call(this);
+
+(function() {
+  udefine('pencilbox/provider/canvas', function() {
+    var CanvasProvider;
+    return CanvasProvider = (function() {
+      function CanvasProvider() {}
+
+      CanvasProvider.prototype.drawRect = function() {};
+
+      return CanvasProvider;
+
+    })();
+  });
+
+}).call(this);
