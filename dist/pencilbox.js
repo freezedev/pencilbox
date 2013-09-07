10db5898a3e8de791322887a508abd0186b4f6f1
Johannes Stein <johannesstein@freeze-dev.com>
2013-09-07 16:24:05 +0200

    Updated dist JS

--- dist/pencilbox.js
+++ dist/pencilbox.js
@@ -20,9 +20,34 @@
   udefine('pencilbox/provider/canvas', function() {
     var CanvasProvider;
     return CanvasProvider = (function() {
-      function CanvasProvider() {}
+      function CanvasProvider(elementId, options) {
+        var elem, height, parent, width;
+        if (elementId == null) {
+          elementId = "canvas-" + (Date.now());
+        }
+        if (options != null) {
+          parent = options.parent, width = options.width, height = options.height;
+        }
+        if (!document.getElementById(elementId)) {
+          elem = document.createElement('canvas');
+          elem.id = elementId;
+          if (width) {
+            elem.width = width;
+          }
+          if (height) {
+            elem.height = height;
+          }
+          if (parent === 'body' || !parent) {
+            document.body.appendChild(elem);
+          } else {
+            document.getElementById(parent).appendChild(elem);
+          }
+        }
+        elem = document.getElementById(elementId);
+        this.context = elem.getContext('2d');
+      }
 
-      CanvasProvider.prototype.drawRect = function() {};
+      CanvasProvider.prototype.drawRect = function(x, y, w, h) {};
 
       return CanvasProvider;
 
